99a61dc92f178b73a0e57b0622543a1dafb35201
AntonioDmit <shustroid@mail.ru>
2023-07-05 11:47:25 +0300

    Task_final

--- task_final/solution.js
+++ task_final/solution.js
@@ -16,6 +16,40 @@
 // - goods: массив объектов с информацией о позициях заказа:
 //     - title - название позиции
 //     - count - количество в заказе
+
+
+function sendRequest(name, phone, address, goods, sum) {
+
+    // let data = {client: name+" "+phone , order: {
+    //     address:"ул. "+address.street+", дом "+address.house+", "+address.entrance+" подъезд, "+address.floor+" этаж, кв. "+address.flat, 
+    //     sum:sum},
+    //     goods: []};
+   
+    // let countOfGoods = goods.length;
+
+    // for (let i = 0; i < countOfGoods; i += 1) {
+    //     data.goods.push({title: goods[i].title, count: goods[i].count});
+    // }
+
+    // let jsonData = JSON.stringify(data);
+    
+    let data = {goods: [], order: {}};
+
+    let countOfGoods = goods.length;
+
+    for (let i = 0; i < countOfGoods; i += 1) {
+        data.goods.push({title: goods[i].title, count: goods[i].count});
+    }
+
+    data.order.address = "ул. "+address.street+", дом "+address.house+", "+address.entrance+" подъезд, "+address.floor+" этаж, кв. "+address.flat;
+    data.order.sum =  sum;
+
+    data.client = name+" "+phone;
+
+    let jsonData = JSON.stringify(data);
+
+    return jsonData;
+}
 // например:
 // {
 //    "data": {
@@ -32,22 +66,3 @@
 //      ]
 //    }
 // }
-
-function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
-    let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
-    return jsonData;
-}
